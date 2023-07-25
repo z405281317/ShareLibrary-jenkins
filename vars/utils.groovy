bc0398cd4acaabfd69ea53e7bdaab3b00f94e358
biance <biance@cg.app>
2023-07-26 00:40:36 +0800

    update123

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -8,6 +8,30 @@ def pull(Branch,Id,GitRepo){
     checkout scmGit(branches: [[name: BRANCH ]], extensions: [], userRemoteConfigs: [[credentialsId: '842ea056-6087-470a-9ca0-06bd1e9fa13c', url: gitRepo]])
 }
 
+//获取变更文件列表，返回HashSet，注意添加的影响文件路径不含仓库目录名
+@NonCPS
+List<String> getChangedFilesList(){
+    def changedFiles = []
+    for ( changeLogSet in currentBuild.changeSets){
+        for (entry in changeLogSet.getItems()){
+            changedFiles.addAll(entry.affectedPaths)
+        }
+    }
+    return changedFiles
+}
+
+// 获取提交ID
+@NonCPS
+String getGitcommitID(){
+    gitCommitID = " "
+    for ( changeLogSet in currentBuild.changeSets){
+        for (entry in changeLogSet.getItems()){
+            gitCommitID = entry.commitId
+        }
+    }
+    return gitCommitID
+}
+
 // 获取提交人
 @NonCPS
 String GetAuthorName(){
